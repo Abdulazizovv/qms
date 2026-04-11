fa4de6b693c86c74613e0595d4ff8dc0a6da5627
mahmudovah <mahmudovahadija.0831@gmail.com>
2026-04-11 15:34:27 +0500

    business admin register

--- business/admin.py
+++ business/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Business, Branch, Operator, Service
 
-# Register your models here.
+
+admin.site.register(Branch)
+admin.site.register(Business)
+admin.site.register(Operator)
+admin.site.register(Service)
