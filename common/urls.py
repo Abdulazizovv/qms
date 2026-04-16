51713c936aae4ecb3186cf99f4dacbd20d5b4236
mahmudovah <mahmudovahadija.0831@gmail.com>
2026-04-16 21:25:29 +0500

    bazi fon ranglari tuzatildi

--- common/urls.py
+++ common/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from business import views
+from .views import home
 
 app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.dashboard_index, name='index'),
+    path('home/', home, name='home')
 ]
